06e5076d9368389702bdb5f81aac8cd9de4f97f4
Rama Chetan <gb365@njit.edu>
2023-02-15 12:50:18 -0500

    First Commit

---
+++ Whatsapp_Scheduler.py
@@ -0,0 +1,30 @@
+import pyautogui
+import time
+
+# Set the phone number and message to send
+phone_number = "+91 93815 72034"
+message = "Hello, World!"
+
+# Set the time to send the message (in 24-hour format)
+send_time = "12:36:00"
+
+# Wait until the send time is reached
+while True:
+    current_time = time.strftime("%H:%M:%S")
+    if current_time == send_time:
+        break
+    time.sleep(1)
+
+# Open the WhatsApp app and click on the new message button
+pyautogui.hotkey('win', 's')
+pyautogui.write('WhatsApp')
+pyautogui.press('enter')
+time.sleep(5)  # wait for WhatsApp to open
+pyautogui.click(200, 200)  # click on the new message button
+
+# Type the phone number and message, and send the message
+pyautogui.write(phone_number)
+pyautogui.press('enter')
+time.sleep(2)  # wait for the chat to open
+pyautogui.write(message)
+pyautogui.press('enter')
